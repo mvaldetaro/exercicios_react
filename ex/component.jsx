24dedd8ef58d099cd4f8d0061c84d77f993e3b13
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-11 17:25:00 -0300

    Criando 2 componete em um único arquivo

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export default props => ( //Ao passar um param não é necessário os parenteses
-  <h1>{props.value}</h1> //expressão, tudo dentro do parenteses será retornado
+const Primeiro = props => (
+  <h1>Primeiro Componente</h1>
 )
+
+const Segundo = props => (
+  <h1>Segundo Componente</h1>
+)
+
+export { Primeiro, Segundo }
 
