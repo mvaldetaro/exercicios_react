1be0771d97b5e0b8dd48f8f7c8be74208fa8a8ec
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-11 17:14:33 -0300

    Aplicando propriedades ao componente

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default () => (
-  <h1>Primeiro Componente!</h1> //expressão, tudo dentro do parenteses será retornado
+export default props => ( //Ao passar um param não é necessário os parenteses
+  <h1>{props.value}</h1> //expressão, tudo dentro do parenteses será retornado
 )
 
